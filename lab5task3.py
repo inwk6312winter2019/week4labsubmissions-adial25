6d3382fa9ac1a82c541dd3421c376a0592003550
adial25 <47114050+adial25@users.noreply.github.com>
2019-02-11 02:48:37 -0400

    Update lab5task3.py

--- lab5task3.py
+++ lab5task3.py
@@ -5,5 +5,5 @@ time.hour=11
 time.minute=59
 time.second=30
 def print_time(time):
-    print("%.2d:%.2d:%.2d"%(time.hour,time.minute,time.sec)
+    print("%.2d:%.2d:%.2d"%(time.hour,time.minute,time.sec))
 print_time(time)  
