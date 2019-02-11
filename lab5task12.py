868a0823578e6e98a234ff9ef73c18bef5c9d57d
adial25 <47114050+adial25@users.noreply.github.com>
2019-02-11 03:24:39 -0400

    subnetting calculator task

--- lab5task12.py
+++ lab5task12.py
@@ -5,11 +5,11 @@ import sys
 
 def subnet_calc():
     try:
-        print "\n"
+        print()
 
         #Checking IP address validity
         while True:
-            ip_address = raw_input("Enter an IP address: ")
+            ip_address = input("Enter an IP address: ")
 
             #Checking octets            
             a = ip_address.split('.')
@@ -18,14 +18,14 @@ def subnet_calc():
                 break
 
             else:
-                print "\nThe IP address is INVALID! Please retry!\n"
+                print ("The IP address is INVALID! Please retry")
                 continue
  
         masks = [255, 254, 252, 248, 240, 224, 192, 128, 0]
 
         #Checking Subnet Mask validity
         while True:
-            subnet_mask = raw_input("Enter a subnet mask: ")
+            subnet_mask = input("Enter a subnet mask: ")
 
             #Checking octets            
             b = subnet_mask.split('.')
@@ -34,7 +34,7 @@ def subnet_calc():
                 break
 
             else:
-                print "\nThe subnet mask is INVALID! Please retry!\n"
+                print("The subnet mask is INVALID! Please retry")
                 continue
 
 	############# Application #1 - Part #2 #############
@@ -149,19 +149,19 @@ def subnet_calc():
         #print broadcast_address
 
         #Results for selected IP/mask
-        print "\n"
-        print "Network address is: %s" % network_address
-        print "Broadcast address is: %s" % broadcast_address
-        print "Number of valid hosts per subnet: %s" % no_of_hosts
-        print "Wildcard mask: %s" % wildcard_mask
-        print "Mask bits: %s" % no_of_ones
-        print "\n"
+      
+        print ("Network address is")
+        print ("Broadcast address is")
+        print ("Number of valid hosts per subnet")
+        print ("Wildcard mask")
+        print ("Mask bits")
+   \fde4
 
         ############# Application #1 - Part #4 #############
 
         #Generation of random IP in subnet
         while True:
-            generate = raw_input("Generate random ip address from subnet? (y/n)")
+            generate = input("Generate random ip address from subnet? (y/n)")
 
             if generate == "y":
                 generated_ip = []
@@ -184,16 +184,15 @@ def subnet_calc():
                 y_iaddr = ".".join(generated_ip)
                 #print y_iaddr
 
-                print "Random IP address is: %s" % y_iaddr
-                print "\n"
+                print ("Random IP address is")
                 continue
 
             else:
-                print "Exiting !\n"
+                print ("Exiting")
                 break
 
     except KeyboardInterrupt:
-        print "\n\nProgram aborted by user. Exiting...\n"
+        print("Program aborted by user. Exitign")
         sys.exit()
 
 #Calling the function
